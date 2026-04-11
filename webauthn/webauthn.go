1ef1a0a9d9cde9e135507307b9e2b8cf00d56146
NekokeCore <core2002@163.com>
2026-04-12 01:21:15 +0800

    fix: 修正 WebAuthn RPID 配置

--- webauthn/webauthn.go
+++ webauthn/webauthn.go
@@ -21,10 +21,11 @@ type Credential = webauthn.Credential
 func Init() {
 	config := &webauthn.Config{
 		RPDisplayName: "FiFu WebAuthn",
-		RPID:          "10.0.3.201:5200",
+		RPID:          "10.0.3.201",
 		RPOrigins: []string{
 			"http://localhost:5200",
 			"http://localhost:5000",
+			"http://10.0.3.201:5200",
 			"https://10.0.3.201:5200",
 		},
 	}
